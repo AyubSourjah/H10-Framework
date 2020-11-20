ec98b323cd9577e54014ea9e434318c0c0e87c8a
Ayub Sourjah <ayub@hsenid.lk>
2020-11-20 09:58:13 +0530

    master

--- src/H10.Data/SettingKeys.cs
+++ src/H10.Data/SettingKeys.cs
@@ -4,10 +4,10 @@ namespace H10.Data
 {
     internal static class SettingKeys
     {
-        public const string RepositoryServer = "SaaS/Server";
-        public const string RepositoryCatalog = "SaaS/Catalog";
-        public const string RepositoryUserName = "SaaS/UserName";
-        public const string RepositoryPassword = "SaaS/Password";
-        public const string RepositoryProvider = "SaaS/Provider";
+        public const string RepositoryServer = "SaaS:Server";
+        public const string RepositoryCatalog = "SaaS:Catalog";
+        public const string RepositoryUserName = "SaaS:UserName";
+        public const string RepositoryPassword = "SaaS:Password";
+        public const string RepositoryProvider = "SaaS:Provider";
     }
 }
