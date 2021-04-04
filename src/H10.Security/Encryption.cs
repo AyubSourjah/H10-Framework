327b08269886164fa517fec4d1c60e48ecbec0a9
Ayub Sourjah <ayub@hsenid.lk>
2021-04-04 08:25:02 +0530

    Adding new Security Encryption Library

---
+++ src/H10.Security/Encryption.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace H10.Security
+{
+    public class Encryption
+    {
+        public string AsymmEncrypt(string value)
+        {
+            return string.Empty;
+        }
+
+        public string AsymmDecrypt(string value)
+        {
+            return string.Empty;
+        }
+
+        public string SymmEncrypt(string value)
+        {
+            return string.Empty;
+        }
+
+        public string SymmDecrypt(string value)
+        {
+            return String.Empty;
+        }
+    }
+}
