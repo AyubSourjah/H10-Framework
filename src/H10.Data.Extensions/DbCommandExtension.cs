c91a4205ab9d1b9046f5a3fb7783edead8ef0b98
AyubSourjah <ayub@hsenidbiz.com>
2023-08-03 17:33:26 +0530

    Refactored

--- src/H10.Data.Extensions/DbCommandExtension.cs
+++ src/H10.Data.Extensions/DbCommandExtension.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Data.Common;
 using System.Dynamic;
 using System.Linq;
 
