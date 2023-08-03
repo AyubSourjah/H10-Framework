3326ee660881c533a76e229dbcf56a035408477b
AyubSourjah <ayub@hsenidbiz.com>
2023-08-03 09:04:21 +0530

    Changed database configuration key names

--- src/H10.Data/SettingKeys.cs
+++ src/H10.Data/SettingKeys.cs
@@ -4,10 +4,10 @@ namespace H10.Data
 {
     internal static class SettingKeys
     {
-        public const string RepositoryServer = "SaaS:Server";
-        public const string RepositoryCatalog = "SaaS:Catalog";
-        public const string RepositoryUserName = "SaaS:UserName";
-        public const string RepositoryPassword = "SaaS:Password";
-        public const string RepositoryProvider = "SaaS:Provider";
+        public const string RepositoryServer = "Database:Server";
+        public const string RepositoryCatalog = "Database:Catalog";
+        public const string RepositoryUserName = "Database:UserName";
+        public const string RepositoryPassword = "Database:Password";
+        public const string RepositoryProvider = "Database:Provider";
     }
 }
